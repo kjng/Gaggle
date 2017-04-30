333dc65f04dcc2cc6075fbf7a380a6866ced203f
Kevin Jang <pianistkevinjang@gmail.com>
2017-04-30 11:22:21 -0400

    Upgrade project

--- android/app/src/main/java/com/gaggle/MainApplication.java
+++ android/app/src/main/java/com/gaggle/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNSoundPackage(),
             new MapsPackage(),
             new ImagePickerPackage()
