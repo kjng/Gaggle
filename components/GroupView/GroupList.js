f013e06d0f532b3e367de848bcf47598cff20a9f
Kevin Jang <pianistkevinjang@gmail.com>
2017-04-30 11:42:42 -0400

    Adjust margins and padding of group icons

--- components/GroupView/GroupList.js
+++ components/GroupView/GroupList.js
@@ -1,77 +1,77 @@
 import React from 'react';
-import { TouchableOpacity, View, Dimensions } from 'react-native';
+import { TouchableOpacity, TouchableWithoutFeedback, View, Dimensions } from 'react-native';
 import { Text, Icon } from 'native-base';
 import stringToColor from './colorGenerator';
 
 const GroupList = ({ _handleChangePage, userGroups, deleteGroup, uid }) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        flexWrap: 'wrap',
-      }}
-    >
-      {userGroups.map((group, i) => {
-        return (
-          <View
-            key={i}
-            style={{
-              shadowColor: 'black',
-              shadowOpacity: 0.7,
-              shadowRadius: 3,
-              shadowOffset: {
-                height: 0,
-                width: 0,
-              },
-              backgroundColor: stringToColor(group),
-              marginTop: 20,
-              justifyContent: 'space-between',
-              width: Dimensions.get('window').width*.45,
-              height: Dimensions.get('window').height*.20,
-              padding: 10,
-              marginLeft: Dimensions.get('window').width*.025,
-              marginRight: Dimensions.get('window').width*.025
-            }}
-          >
-            <TouchableOpacity
-              onPress={() => _handleChangePage(group || '')}
-            >
-              <Text
-                style={{
-                  fontSize: 25,
-                  fontWeight: '600',
-                }}
-              >{group}</Text>
-            </TouchableOpacity>
+    <View style={{ marginLeft: 13, marginRight: 12 }}>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          flexWrap: 'wrap',
+        }}
+      >
+        {userGroups.map((group, i) => {
+          return (
             <View
+              key={i}
               style={{
-                position: 'absolute',
-                bottom: 13,
-                right: 13,
-                width: 50,
-                height: 50,
-                borderRadius: 50,
-                backgroundColor: 'white',
+                shadowColor: 'black',
+                borderRadius: 15,
+                backgroundColor: stringToColor(group),
+                marginTop: 10,
+                justifyContent: 'space-between',
+                width: Dimensions.get('window').width * 0.452,
+                height: Dimensions.get('window').height * 0.20,
+                padding: 10,
               }}
             >
-              <Icon
-                name={'trash'}
-                style={{
-                  color: 'red',
-                  position: 'absolute',
-                  bottom: 7,
-                  right: 16,
-                }}
+              <TouchableOpacity
+                onPress={() => _handleChangePage(group || '')}
+              >
+                <Text
+                  style={{
+                    fontSize: 25,
+                    fontWeight: '600',
+                    color: 'white',
+                  }}
+                >{group}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
                 onPress={() => deleteGroup(uid, group)}
-              />
+              >
+                <View
+                  style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    right: 0,
+                    width: 40,
+                    height: 40,
+                    borderRadius: 50,
+                    backgroundColor: 'white',
+                  }}
+                >
+                  <Icon
+                    name={'trash'}
+                    style={{
+                      color: 'red',
+                      position: 'absolute',
+                      bottom: 6,
+                      right: 13,
+                      fontSize: 23,
+                    }}
+                  />
+                </View>
+              </TouchableOpacity>
             </View>
-          </View>
-        );
-      },
-      )
-      }
+          );
+        },
+        )
+        }
+      </View>
     </View>
   );
 };
